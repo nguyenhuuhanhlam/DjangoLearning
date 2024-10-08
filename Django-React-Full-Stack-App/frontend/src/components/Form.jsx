c3330d0167993b3306ed6424e6f20f4c1b5ea08a
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2024-10-09 00:24:50 +0700

    ...

--- Django-React-Full-Stack-App/frontend/src/components/Form.jsx
+++ Django-React-Full-Stack-App/frontend/src/components/Form.jsx
@@ -1,3 +1,10 @@
-import { useStat } from "react"
+import { useState } from "react"
 import api from "../api"
 import { useNavigate } from "react-router-dom"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants"
+
+const Form = ({ route, method }) => {
+	const [username, setUsername] = useState("")
+	const [password, setPassword] = useState("")
+	const [loading, setLoading] = useState(false)
+}
