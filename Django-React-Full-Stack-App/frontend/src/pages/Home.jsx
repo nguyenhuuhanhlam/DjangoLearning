38cb735f1b6c5b80e66d0176b0af8ccb397c820d
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2024-10-09 19:37:20 +0700

    ...

--- Django-React-Full-Stack-App/frontend/src/pages/Home.jsx
+++ Django-React-Full-Stack-App/frontend/src/pages/Home.jsx
@@ -1,7 +1,20 @@
-import {} from 'react'
+import { useState, useEffect } from "react"
+import api from "../api"
 
 const Home = () => {
-    return <div>Home</div>
+	const [notes, setNotes] = useState([])
+	const [content, setContent] = useState("")
+	const [title, setTitle] = useState("")
+
+	const getNotes = () => {
+		api
+			.get("/api/notes")
+			.then((res) => res.data)
+			.then((data) => setNotes(data))
+			.catch((err) => alert(err))
+	}
+
+	return <div>Home</div>
 }
 
 export default Home
