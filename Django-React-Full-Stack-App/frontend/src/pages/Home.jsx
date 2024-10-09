f3b998253b6f733fc1eebb66c7b704033081af20
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2024-10-09 23:05:26 +0700

    ...

--- Django-React-Full-Stack-App/frontend/src/pages/Home.jsx
+++ Django-React-Full-Stack-App/frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import Note from "../components/Note"
 import api from "../api"
 
 const Home = () => {
@@ -18,7 +19,63 @@ const Home = () => {
 			.catch((err) => alert(err))
 	}
 
-	return <div>Home</div>
+	const createNote = (e) => {
+		e.preventDefault()
+		api
+			.post("/api/notes/", { content, title })
+			.then((res) => {
+				if (res.status === 201) alert("Note created!")
+				else alert("Failed to make note.")
+				getNotes()
+			})
+			.catch((err) => alert(err))
+	}
+
+	const deleteNote = (id) => {
+		api
+			.delete(`/api/notes/delete/${id}/`)
+			.then((res) => {
+				if (res.status === 204) alert("Note deleted!")
+				else alert("Failed to delete note.")
+				getNotes()
+			})
+			.catch((error) => alert(error))
+	}
+
+	return (
+		<div>
+			<div>
+				<h2>Notes</h2>
+				{notes.map((note) => (
+					<Note note={note} onDelete={deleteNote} key={note.id} />
+				))}
+			</div>
+			<h2>Create a Notes</h2>
+			<form onSubmit={createNote}>
+				<label htmlFor="title">Title:</label>
+				<br />
+				<input
+					type="text"
+					id="title"
+					name="title"
+					required
+					onChange={(e) => setTitle(e.target.value)}
+					value={title}
+				/>
+				<label htmlFor="content">Content:</label>
+				<br />
+				<textarea
+					id="content"
+					name="content"
+					required
+					value={content}
+					onChange={(e) => setContent(e.target.value)}
+				></textarea>
+				<br />
+				<input type="submit" value="Submit"></input>
+			</form>
+		</div>
+	)
 }
 
 export default Home
